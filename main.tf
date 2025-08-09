5141cf7e0a67d272583a01bbcc18648a396a89e0
Aisalkyn85 <87104292+Aisalkyn85@users.noreply.github.com>
2025-08-09 09:17:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,21 +3,18 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  subnet_id                   = var.subnet_id
-  key_name                    = var.key_name
+  count         = var.instance_count
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
 
   tags = {
     Name = "ExampleInstance-${count.index + 1}"
   }
 
-  count = var.instance_count
-
   lifecycle {
     prevent_destroy = false
   }
 }
-
-
 
